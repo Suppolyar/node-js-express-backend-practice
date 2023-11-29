d3f44ef1118c3fcc581690870d9af493293df3a4
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2023-11-29 17:13:58 +0300

    feat: added a Users route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import {userRouter} from "./users/users.js";
 
 const port = 8000
 const app = express()
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+    res.end()
 })
+
+app.use('/users', userRouter)
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`)
